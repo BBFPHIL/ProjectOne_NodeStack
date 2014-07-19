4f74fa7d5c851587cd7bb31ebbcc62bcf05ca73b
BBFPHIL <wunderwalk24@gmail.com>
2014-07-19 08:28:10 -0400

    Synchronous / Asychronous file writing

---
+++ filesystem/file_write_sync.js
@@ -0,0 +1,12 @@
+
+var fs = require('fs');
+var veggieTray = ['carrots', 'celery', 'olives'];
+fd = fs.openSync('../filesystem/config.txt', 'w');
+
+while (veggieTray.length){
+	veggie = veggieTray.pop() + " ";
+	var bytes = fs.writeSync(fd, veggie, null, null);
+	console.log("Wrote: %s %dbytes", veggie, bytes);
+}
+
+fs.closeSync(fd);
