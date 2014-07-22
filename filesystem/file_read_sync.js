92eee94fcdf00b6b82d277f455f70be6cca4a113
BBFPHIL <wunderwalk24@gmail.com>
2014-07-22 10:09:11 -0400

    Read files async and sync

---
+++ filesystem/file_read_sync.js
@@ -0,0 +1,16 @@
+
+var fs = require('fs');
+fd = fs.openSync('../filesystem/veggie.txt', 'r');
+
+var veggies = "";
+
+do {
+	var buf = new Buffer(5);
+	buf.fill();
+	var bytes = fs.readSync(fd, buf, null, 5);
+	console.log("read %dbytes", bytes);
+	veggies += buf.toString();
+}while (bytes > 0);
+
+fs.closeSync(fd);
+console.log("Veggies: " + veggies);
